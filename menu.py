55e72dc1da4d0e57b897c8b63ee3d463235a519f
Rattatwinko <89251490+ZockerKatze@users.noreply.github.com>
2025-01-25 14:56:35 +0100

    Add files via upload

--- menu.py
+++ menu.py
@@ -4,24 +4,61 @@ import tkinter as tk
 from tkinter import ttk
 from debug import readPID
 import threading
+import subprocess
+
+htop = True
 
 
 def display_window():
+    global htop
+
     def quitfunction():
         root.destroy()
         exit()
 
     def button1_action():
-        run_nl_canvas()
+        nlthread = threading.Thread(target=run_nl_canvas(), daemon=True)
+        nlthread.start()
         root.destroy()  # Should destroy window
         print("Game Launched (box.py)")
 
     def button2_action():
-        run_NOL()
+        nolthread = threading.Thread(target=run_NOL(), deamon=True)
+        nolthread.start()
         root.destroy()
 
-    #def debug_action():
-                            # make a new thread here because the debug was cockblocking the tkinter app
+    def debug_action():
+
+        """
+
+        this will not properly work!
+        if you run debug through menu it will continue running when exited
+        idk why
+
+        If you want to enable the real "debug file" that works then uncomment the debug_thread var
+        and comment out starting htop and cleanuphtop!
+
+        """
+
+
+        if htop:
+            def run_htop():
+                global htop_process
+                htop_process = subprocess.Popen(["htop"])
+            ndbgth = threading.Thread(target=run_htop(), daemon=True)
+            ndbgth.start()
+
+            atexit.register(cleanup_htop)
+
+        else:
+            pass
+
+        def cleanup_htop():
+            if "htop_process" in globals() and htop_process.poll() is None:
+                htop_process.terminate()
+
+
+        # make a new thread here because the debug was cockblocking the tkinter app
 
     #    debug_thread = threading.Thread(target=readPID, daemon=True)
     #    debug_thread.start()
